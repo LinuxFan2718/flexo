5226734e5f4daa198b34161bfd65cf0876c57bb7
Dennis Cahillane <dennis@thirtymadison.com>
2021-03-29 01:14:59 -0400

    minor tweaks

--- flexo.py
+++ flexo.py
@@ -1,10 +1,12 @@
 import fileinput
 import time
 import sys
+import chess
+import random
 
 timestamp = str(int(time.time()))
-logfile = timestamp + "flexo.log"
-log = open(logfile, "x")
+logfile = "flexo" + timestamp + ".log"
+log = open(logfile, "a", 0)
 
 for line in fileinput.input():
   log.write(line)
@@ -19,11 +21,19 @@ for line in fileinput.input():
   elif(command == "ucinewgame"):
     pass
   elif(command.startswith("go")):
+    # extract the FEN from the command
+    # create a chess Board with that FEN, or startpos
+    board = chess.Board()
+    legal_moves = list(board.legal_moves)
+    random_move = random.choice(list(legal_moves))
+    print(f"bestmove {random_move.uci()}")
     pass
   elif(command.startswith("position")):
     pass
   elif(command == "stop"):
     print("bestmove e2e4")
+  elif(command.startswith("setoption")):
+    pass
   elif (command in ["quit","exit"]):
     log.close()
     sys.exit(0)
