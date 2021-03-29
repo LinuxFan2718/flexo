a7bcb6f976ef051d3f4c6d980238e62f74e64f45
Dennis Cahillane <dennis@thirtymadison.com>
2021-03-29 00:54:29 -0400

    adds more useful code

--- bot.py
+++ bot.py
@@ -7,5 +7,12 @@ board = chess.Board()
 legal_moves = list(board.legal_moves)
 # select one at random
 random_move = random.choice(list(legal_moves))
-print(random_move.uci())
-# sample output: g2g4
+print(random_move.uci()) # sample output: g2g4
+# make that move
+board.push(random_move)
+legal_moves2 = list(board.legal_moves)
+random_move2 = random.choice(list(legal_moves2))
+print(random_move2.uci())
+board.push(random_move2)
+print(board)
+pass
