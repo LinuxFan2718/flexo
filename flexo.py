5c9133844fffe87922b2553ff4a30d9018c3c1ad
Dennis Cahillane <dennis@thirtymadison.com>
2021-04-13 01:43:54 -0400

    make a random legal move from any position

--- flexo.py
+++ flexo.py
@@ -23,16 +23,19 @@ for line in stdin_fileno:
   elif(command == "ucinewgame"):
     pass
   elif(command.startswith("go")):
-    # access the chess board and choose a random move
-    board = chess.Board()
     legal_moves = list(board.legal_moves)
     random_move = random.choice(list(legal_moves))
     stdout_fileno.write(f"bestmove {random_move.uci()}" + '\n')
   elif(command.startswith("position")):
-    # extract the FEN from the command
-    # create a chess Board with that FEN, or startpos
-    # store the chess board to be ready for when go is called
-    pass
+    # position startpos
+    # position startpos moves g1f3 e7e5 b1a3 d7d5 b2b3 d8h4 h2h3 e5e4 e2e3 e4f3
+    board = chess.Board()
+    tokens = command.split(' ')
+    if len(tokens) > 2:
+      uci_moves = tokens[3:]
+      for uci_move in uci_moves:
+        move = chess.Move.from_uci(uci_move)
+        board.push(move)
   elif(command == "stop"):
     pass
     # maybe call go function instead
